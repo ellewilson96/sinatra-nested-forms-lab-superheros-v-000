c6c1b8711bb97cd801e091545e7f7591449144d6
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-12 22:14:42 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,1 +1,11 @@
-class Superheroes
+class Superhero
+  attr_reader :name, :power, :bio 
+
+SUPERHEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    SUPERHEROES << self
+  end
